1583bfa87848e8eeb13e252b5af01290bd1ad4a8
Oisee Hastin <hastin@oisee.cool>
2023-04-18 01:09:05 +0800

    explaning

--- js/main.js
+++ js/main.js
@@ -1056,7 +1056,7 @@ let databaseStatusBtn = document.getElementById("databaseStatusBtn");
 let databaseLoadingContent =
      '<button class="btn btn-secondary btn-sm" type="button" disabled><span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only"> Loading database...</span></button>';
 let databaseLoadedContent =
-     '<button class="btn btn-success btn-sm" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16"><path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/></svg></button>';
+     '<button class="btn btn-success btn-sm" type="button" data-bs-toggle="tooltip"	data-bs-placement="top"	data-bs-html="true"	title="重新讀取資料庫"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16"><path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/></svg></button>';
 
 const dataSheetUrl = "https://script.google.com/macros/s/AKfycbwi7DtotOCj-7xbk7h3qzgSCobwaRr5-mXGNI_oG6OSjNfa9SXpjtI_47UnwqiGy65Kag/exec";
 document.addEventListener("DOMContentLoaded", init);
